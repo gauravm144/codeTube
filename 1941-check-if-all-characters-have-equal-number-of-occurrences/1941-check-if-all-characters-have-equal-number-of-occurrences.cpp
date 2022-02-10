e33471e5fef9bbcb291af648ab492683aa771a20
gauravm144 <66792745+gauravm144@users.noreply.github.com>
2022-02-10 16:15:21 +0530

    Time: 4 ms (75.18%), Space: 6.8 MB (60.79%) - LeetHub

---
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool areOccurrencesEqual(string s) {
+        int ar[26]={0};
+        int max=0;
+        for(int i=0;i<s.length();i++)
+        {
+            ar[s[i]-'a']+=1;
+            if(max<ar[s[i]-'a'])
+            max=ar[s[i]-'a'];
+        }
+        // for(int i=0;i<26;i++)
+            // cout<<ar[i]<<endl;
+        cout<<max;
+        for(int i=0;i<26;i++)
+        {
+            if(ar[i]!=0&&max!=ar[i])
+                return false;
+                
+        }
+        return true;
+        
+    }
+};
