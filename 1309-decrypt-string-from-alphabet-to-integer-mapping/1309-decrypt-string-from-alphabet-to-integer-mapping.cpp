c156441bff32caa868903d79475855c3c36a5a1c
gauravm144 <66792745+gauravm144@users.noreply.github.com>
2022-02-10 11:22:46 +0530

    Time: 14 ms (5.13%), Space: 7.1 MB (8.79%) - LeetHub

---
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string freqAlphabets(string s) {
+        string s1="";
+        int ss=0;
+        char ch;
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            if(s[i]=='#')
+            {
+                i--;
+                i--;
+                
+                ss=((s[i]-'0')*10)+s[i+1]-'0';
+                cout<<ss<<endl;
+                ch='a'+ss-1;
+                s1=ch+s1;
+                continue;
+            }
+            ss=s[i]-'0';
+            // cout<<ss<<endl;
+            ch='a'+ss-1;
+            cout<<s[i]<<"  "<<ch<<endl;
+            s1=ch+s1;
+        }
+        return s1;
+        
+    }
+};
