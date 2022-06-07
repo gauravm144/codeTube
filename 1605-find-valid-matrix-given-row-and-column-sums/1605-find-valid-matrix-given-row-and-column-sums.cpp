2afa89ca8e56c2a97a6e4bb3c0aeba9c16567ac1
gauravm144 <66792745+gauravm144@users.noreply.github.com>
2022-06-07 14:59:18 +0530

    Time: 126 ms (32.25%), Space: 41.7 MB (7.20%) - LeetHub

---
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+        
+        vector<vector<int>> vtk;
+        int n1=rowSum.size();
+        int n2=colSum.size();
+        for(int i=0;i<n1;i++)
+        {
+         vector<int> tmp(n2,0);
+            vtk.push_back(tmp);
+        }
+        int i=0,j=0;
+        while(i<n1&&j<n2)
+        {
+            if(rowSum[i]<colSum[j])
+            {
+                vtk[i][j]=rowSum[i];
+                colSum[j]-=rowSum[i];
+                rowSum[i]=0;
+                i++;
+            }
+            else if(rowSum[i]>colSum[j])
+            {
+                vtk[i][j]=colSum[j];
+                rowSum[i]-=colSum[j];
+                colSum[j]=0;
+                j++;
+            }
+            else if(rowSum[i]==colSum[j])
+            {
+                vtk[i][j]=rowSum[i];
+                colSum[j]-=rowSum[i];
+                rowSum[i]-=colSum[j];
+                i++;
+                j++;
+            }
+        }
+        return vtk;
+    }
+};
