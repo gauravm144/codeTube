196b0546ac0765fedc50f8766264da59e07b25ff
gauravm144 <66792745+gauravm144@users.noreply.github.com>
2022-07-07 09:35:28 +0530

    Time: 3 ms (53.86%), Space: 7.7 MB (47.18%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -3,17 +3,11 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         int prev=nums[0];
-        int pick=0;
-        int dont=0;
         int prev2=0;
         for(int i=1;i<n;i++)
         {
-            pick=nums[i];
-            
-            if(i>1)
-            pick+=prev2;
-            
-            dont=prev;
+            int pick=nums[i]+prev2;
+            int dont=prev;
             prev2=prev;
             prev=max(pick,dont);
         }
