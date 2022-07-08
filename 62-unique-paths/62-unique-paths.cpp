81430982e20eb4aa10ac821366b8c09cdc0ddf68
gauravm144 <66792745+gauravm144@users.noreply.github.com>
2022-07-08 10:01:58 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (25.65%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int findPath(int i,int j,int m,int n,vector<vector<int>> &vtk)
+    int find(int m,int n,vector<vector<int>>&dp)
     {
-       
-      
-        if(i>=m||j>=n)
-            return 0;
-        else if(vtk[i][j]!=-1)
-            return vtk[i][j];
-        else if(i==m-1&&j==n-1)
-            return 1;
-       int a=findPath(i+1,j,m,n,vtk);
-        int b=findPath(i,j+1,m,n,vtk);
-        int c=a+b;
-        vtk[i][j]=c;
-        return c;
         
+         if(m==0&&n==0)
+            return 1;
+        
+        if(m<0||n<0)
+            return 0;
+        
+          if(dp[m][n]!=-1)
+            return dp[m][n];
+        
+        int up=find(m-1,n,dp);
+        int left=find(m,n-1,dp);
+        dp[m][n]=up+left;
+        return dp[m][n];
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> vtk(m,vector<int>(n,-1));
-        
-        int x=findPath(0,0,m,n,vtk); 
-        return x;
+        vector<vector<int>> dp(m,vector<int> (n,-1));
+       int c=find(m-1,n-1,dp);
+        return c;
     }
 };
