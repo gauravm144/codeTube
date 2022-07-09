a86b0516884b1c48a85a4199be10da81db368315
gauravm144 <66792745+gauravm144@users.noreply.github.com>
2022-07-09 21:28:28 +0530

    Time: 494 ms (42.97%), Space: 130.3 MB (97.03%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,37 +1,36 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-     
-    int n=nums.size();
-    vector<int> nge(n);
-    nge[n-1]=n;
-    stack<int> stk;
-    stk.push(n-1);
-    for(int i=n-2;i>=0;i--)
-    {
-        while(stk.size()>0&&nums[i]>=nums[stk.top()])
-            stk.pop();
-    
-         if(stk.size()==0)
-             nge[i]=n;
-        else
-            nge[i]=stk.top();
-        
-        stk.push(i);
-    }
-    int j=0;
-    for(int i=0;i<n-k+1;i++)
-    {
-        if(j<i)
-            j=i;
-        while(nge[j]<i+k)
-            j=nge[j];
-        nums[i]=nums[j];
-    }
-    k--;
-    while(k--)
-        nums.pop_back();
-        return nums;
-        
+        int n=nums.size();
+        vector<int> right(n);
+        stack<int> stk;
+        stk.push(n-1);
+        right[n-1]=n;
+        for(int i=n-2;i>=0;i--)
+        {
+            while(stk.size()>0&&nums[i]>=nums[stk.top()])
+                stk.pop();
+            if(stk.size()==0)
+                right[i]=n;
+            else
+                right[i]=stk.top();
+            
+            stk.push(i);
+        }
+        int j=0;
+        for(int i=0;i<n-k+1;i++)
+        {
+            if(j<i)
+                j=i;
+            while(right[j]<i+k)
+            {
+                j=right[j];
+                
+            }
+            right[i]=nums[j];
+        }
+        for(int i=1;i<k;i++)
+            right.pop_back();
+        return right;
     }
 };
