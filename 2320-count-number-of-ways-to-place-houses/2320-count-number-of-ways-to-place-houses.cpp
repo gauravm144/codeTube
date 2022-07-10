e8ad4ee310be66aa1a291f3bfaa2c4aae7706b2b
gauravm144 <66792745+gauravm144@users.noreply.github.com>
2022-07-10 17:11:19 +0530

    Time: 16 ms (41.23%), Space: 10.5 MB (35.13%) - LeetHub

---
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mod=1e9+7;
+    int find(int n,vector<int> &dp)
+    {
+        if(n==0||n==-1)
+            return 1;
+        if(dp[n]!=-1)
+            return dp[n];
+        
+        long long a=find(n-1,dp);
+        long long b=find(n-2,dp);
+        
+        return dp[n]=(a+b)%mod;
+    }
+    int countHousePlacements(int n) {
+        vector<int> dp(n+1,-1);
+        long long c=find(n,dp);
+        c=c*c;
+        c=c%mod;
+        return (int)c;
+    }
+};
