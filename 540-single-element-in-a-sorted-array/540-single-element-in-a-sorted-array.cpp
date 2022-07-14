60e2d9103fb5e5e85fe3f16cee5420a0ca96a223
gauravm144 <66792745+gauravm144@users.noreply.github.com>
2022-07-14 11:07:24 +0530

    Time: 32 ms (78.96%), Space: 22.4 MB (9.27%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -5,19 +5,13 @@ public:
         while(low<=high)
         {
             int mid=(low+high)/2;
-            if(mid%2==0)
+            if(nums[mid]==nums[mid^1])
             {
-                if(nums[mid]==nums[mid+1])
-                    low=mid+1;
-                else
-                    high=mid-1;
+                low=mid+1;
             }
             else
             {
-                if(nums[mid]==nums[mid-1])
-                    low=mid+1;
-                else
-                    high=mid-1;
+                high=mid-1;
             }
         }
         return nums[low];
